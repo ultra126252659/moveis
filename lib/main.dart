3ae2af3edfb846bd09621cb5298372a73f3a3bd4
ultra126252659 <gabra3637@gmail.com>
2026-03-07 23:48:32 +0200

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,45 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:moves_final_project/features/presentation/Screen/Onboarding.dart';
-import 'package:moves_final_project/features/presentation/Screen/auth/login_screen.dart';
-import 'package:moves_final_project/features/presentation/Screen/auth/register_screen.dart';
-import 'package:moves_final_project/features/presentation/Screen/auth/reset_password_screen.dart';
-import 'package:moves_final_project/features/presentation/Screen/splash_Screen.dart';
-import 'package:moves_final_project/features/presentation/providers/auth_provider.dart';
-
-import 'package:moves_final_project/firebase_options.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:moves_final_project/core/resources/auto_route.dart';
+import 'package:moves_final_project/di.dart';
+import 'package:moves_final_project/features/auth/providers/auth_provider.dart';
+import 'package:moves_final_project/features/details/presentation/screen/movie_details_screen.dart';
+import 'package:moves_final_project/features/home/presentation/screen/home_screen.dart';
 import 'package:provider/provider.dart';
 
-Future<void> main() async {
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
+  configureDependencies();
   runApp(
       MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => AuthProvider()),
-
-        ],
-        child: const MyApp(),
-      ),
-  );
+          providers: [
+            ChangeNotifierProvider(create: (_) => AuthProvider()),
+          ],
+          child:MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+    MyApp({super.key});
+   final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-       initialRoute: Splashscreen.routeName,
-      routes: {
-        Splashscreen.routeName: (c) => Splashscreen(),
-        MoviesIntroScreen.routeName: (c) => MoviesIntroScreen(),
-        LoginScreen.routeName: (c) => LoginScreen(),
-        RegisterScreen.routeName: (c) => RegisterScreen(),
-        ResetPasswordScreen.routeName: (c) => ResetPasswordScreen(),
-    },
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+      builder: (context,child){
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: _appRouter.config(),
+          );
+      },
     );
   }
 }
+
 
 
